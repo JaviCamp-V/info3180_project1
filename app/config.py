d855c4ba278859974aae4f264c4d8e973e24f5ca
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-03-22 23:31:08 -0500

    Re try

--- app/config.py
+++ app/config.py
@@ -3,9 +3,9 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
-    #SQLALCHEMY_DATABASE_URI='postgresql://hllebyohjzlfgz:c575c24eb703b56c7b873c04d0fa36e9cf1d9d027ed59bfc0540bcb56c20030c@ec2-3-91-127-228.compute-1.amazonaws.com:5432/dapcgoc2a7vob6'
+    SECRET_KEY ='Som3$ec5etK*y'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
+    SQLALCHEMY_DATABASE_URI='postgresql://ujdfdgbvpynvxl:df7d7be9238484017aef112502528d6b80a11bb45e74aafe4d7a4bef816db709@ec2-54-145-249-177.compute-1.amazonaws.com:5432/d2d8u8ek7hfoqj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './app/static/img'
     #####3set DATABASE_URL=postgresql://project1:project1@localhost/project1
