ccd537d50baf00ac62bc6124f49440bc89274a72
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-03-23 23:04:40 -0500

    Edit 403

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI='postgresql://ujdfdgbvpynvxl:df7d7be9238484017aef112502528d6b80a11bb45e74aafe4d7a4bef816db709@ec2-54-145-249-177.compute-1.amazonaws.com:5432/d2d8u8ek7hfoqj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './uploads'
-    #####3set DATABASE_URL=postgresql://info3180-project1:info3180-project1@localhost/info3180-project1
+    ######3set DATABASE_URL=postgresql://info3180-project1:info3180-project1@localhost/info3180-project1
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
