ee7255fe0f621f7079833023a7b37c6bd9ddcedd
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-03-23 22:31:03 -0500

    Edit 402

--- app/config.py
+++ app/config.py
@@ -4,11 +4,11 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY ='Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
-    #SQLALCHEMY_DATABASE_URI='postgresql://ujdfdgbvpynvxl:df7d7be9238484017aef112502528d6b80a11bb45e74aafe4d7a4bef816db709@ec2-54-145-249-177.compute-1.amazonaws.com:5432/d2d8u8ek7hfoqj'
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  
+    SQLALCHEMY_DATABASE_URI='postgresql://ujdfdgbvpynvxl:df7d7be9238484017aef112502528d6b80a11bb45e74aafe4d7a4bef816db709@ec2-54-145-249-177.compute-1.amazonaws.com:5432/d2d8u8ek7hfoqj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
-    UPLOAD_FOLDER = './app/static/img'
-    #####3set DATABASE_URL=postgresql://project1:project1@localhost/project1
+    UPLOAD_FOLDER = './uploads'
+    #####3set DATABASE_URL=postgresql://info3180-project1:info3180-project1@localhost/info3180-project1
 
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
